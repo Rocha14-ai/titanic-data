0a6e4046ac94b1841e51929ea5d19c7f9d6fc89a
Rocha14-ai <157132701+Rocha14-ai@users.noreply.github.com>
2024-02-22 06:40:58 -0700

    cambio 1

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-echo "backend: TkAgg" >> ~/.matplotlib/matplotlibrc
+get_backend(echo "backend: TkAgg" >> ~/.matplotlib/matplotlibrc)
 
 st.title("""
 HOLA MUNDO
