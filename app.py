627b93453a17f6fbd0d62934ef636de9d2e24f30
Rocha14-ai <157132701+Rocha14-ai@users.noreply.github.com>
2024-02-22 06:25:03 -0700

    cambio 1

--- app.py
+++ app.py
@@ -16,9 +16,10 @@ st.dataframe(df)
 chart_data = pd.DataFrame(
    {
        "col1": df["Survived"]
+       "col2": range(0,2)
 
    }
 )
 
-st.bar_chart(chart_data, x="col1")
+st.bar_chart(chart_data, x="col2", y="col1")
 
