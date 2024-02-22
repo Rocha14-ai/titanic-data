60943d50086ccfcb99e703e47724e5fa84459d94
Rocha14-ai <157132701+Rocha14-ai@users.noreply.github.com>
2024-02-22 06:29:45 -0700

    cambio 1

--- app.py
+++ app.py
@@ -12,14 +12,7 @@ st.markdown("*juan*")
 df = pd.read_csv('train.csv')
 st.dataframe(df)
 
+chart_data = pd.DataFrame(df["Survived"], columns=["a"])
 
-chart_data = pd.DataFrame(
-   {
-       "col1": df["Survived"],
-       "col2": range(0,2)
-
-   }
-)
-
-st.bar_chart(chart_data, x="col2", y="col1")
+st.bar_chart(chart_data)
 
